25583662df7ce721e05e55377727cf373417262f
Luiz Miyazaki <luiz.miyazaki@unico.io>
2022-07-13 17:40:47 -0300

    first commit

---
+++ sample-app/sample-app/AppConfig.swift
@@ -0,0 +1,8 @@
+//
+//  AppConfig.swift
+//  sample-app
+//
+//  Created by Luiz Massao Miyazaki on 20/05/22.
+//
+
+import Foundation
---
+++ sample-app/sample-app/AppCustomization.swift
@@ -0,0 +1,8 @@
+//
+//  AppCustomization.swift
+//  sample-app
+//
+//  Created by Luiz Massao Miyazaki on 20/05/22.
+//
+
+import Foundation
